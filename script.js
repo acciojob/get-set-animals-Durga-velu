e609af303f5c12789ce093f10efc3ff09701a093
Durga-velu <durgadevi134babu@gmail.com>
2025-03-25 00:02:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species
+	}
+	makeSound(){
+		console.log(`The ${species} makes a sound`)
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
 
-class Cat extends Animal {}
+	constructor(species){
+		this.species=species
+	}
+
+purr(){
+	console.log("purr")
+}
+	
+}
+
+class Cat extends Animal {
+
+		constructor(species){
+		this.species=species
+	}
+
+	bark(){
+		console.log("woof")
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
